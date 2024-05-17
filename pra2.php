3470496080199576135d81023c32e8e35d71178b
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-05-17 14:45:58 +0300

    Update pra2.php

--- pra2.php
+++ pra2.php
@@ -1,6 +1,6 @@
 <?php
 
-function arrayUnique(array $arr): string
+function arrayUnique(array $arr): array
 {
     for ($i = 0; $i < count($arr); $i++) {
         for ($j = $i + 1; $j < count($arr); $j++) {
@@ -10,15 +10,7 @@ function arrayUnique(array $arr): string
         }
     }
   
-    $result = "";
-    for ($i = 0; $i < count($arr); $i++) {
-        $result .= $arr[$i];
-        if ($i < count($arr) - 1) {
-            $result .= " ";
-        }
-    }
-  
-    return $result;
+    return array_values($arr);
 }
 
 $inputArray = [56, 56, 56, 56, 67, 89, 67, 345, 345, 67, 34];
@@ -28,6 +20,6 @@ echo "Массив в коде:\n";
 print_r($inputArray);
 
 echo "\nМассив без дубликатов:\n";
-echo $uniqueArray;
+print_r($uniqueArray);
 
 ?>
